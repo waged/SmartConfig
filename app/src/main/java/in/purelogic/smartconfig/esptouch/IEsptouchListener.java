cbb5c23b753abb4ce9a91d83800a164e2bafdac4
mac <abdelwagedmohamed@gmail.com>
2018-08-23 19:23:33 +0530

    init. commit

---
+++ app/src/main/java/in/purelogic/smartconfig/esptouch/IEsptouchListener.java
@@ -0,0 +1,11 @@
+package in.purelogic.smartconfig.esptouch;
+
+public interface IEsptouchListener {
+    /**
+     * when new esptouch result is added, the listener will call
+     * onEsptouchResultAdded callback
+     *
+     * @param result the Esptouch result
+     */
+    void onEsptouchResultAdded(IEsptouchResult result);
+}
